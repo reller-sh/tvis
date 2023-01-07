376d5213123365b09bb7deadd807379d3e1e6dc0
rest <vlad.shkerdin@relsys.eu>
2023-01-07 16:46:59 +0300

    add lab 6 adopt lab 1 for 6

--- src/lab1/index.ts
+++ src/lab1/index.ts
@@ -12,13 +12,11 @@ import fp from 'lodash/fp'
 
 
 fs.open('/tmp/test', fs.constants.O_RDONLY | fs.constants.O_NONBLOCK, (err: any, fd: any) => {
-    // Handle err
-    const pipe = new net.Socket({ fd });
-    // Now `pipe` is a stream that can be used for reading from the FIFO.
-    pipe.on('data', (data: any) => {
-        // console.log(data.toString())
-        console.log(fp.orderBy(fp.toNumber, ['asc'], JSON.parse(data)))
 
+    const pipe = new net.Socket({ fd });
+
+    pipe.on('data', (data: any) => {
+        console.log(fp.orderBy(fp.toNumber, ['asc'], JSON.parse(data)))
     });
 });
 
